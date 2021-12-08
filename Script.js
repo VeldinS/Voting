684583f57627ac104534f7f95bdb80317e9ea4c0
VeldinS <190302158@student.ius.edu.ba>
2021-12-08 19:25:05 +0100

    2

--- Script.js
+++ Script.js
@@ -3,6 +3,4 @@
     location.href = "https://veldins.github.io/Voting/Page2";
     };
 
-    document.getElementById("button2").onclick = function () {
-        location.href = "https://veldins.github.io/Voting/Index";
-    };
+    
---
+++ Script2.js
@@ -0,0 +1,3 @@
+document.getElementById("button2").onclick = function () {
+    location.href = "https://veldins.github.io/Voting/Index";
+};
